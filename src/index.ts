eee05a1b15f071e7ad7abd134c1de86ba432fd9f
eric <73148494+ericzhu98@users.noreply.github.com>
2021-10-08 01:02:03 +1100

    added permissions for specific role

--- src/index.ts
+++ src/index.ts
@@ -5,27 +5,25 @@ const { token } = require('./config.json');
 
 const client = new Client({ intents: [Intents.FLAGS.GUILDS] })
 
-client.once('ready', () => {
-    console.log('Ready!')
-})
-
-client.on('interactionCreate', async interaction => {
-    if (!interaction.isCommand()) return;
-
+client.once('ready', async () => {
     if (!client.application?.owner) await client.application?.fetch();
 
-    const command = await client.guilds.cache.get('123456789012345678')?.commands.fetch('876543210987654321');
+    const command = await client.guilds.cache.get('860941635047522374')?.commands.fetch('895574855666245672');
 
     const permissions = [
         {
-            id: '201930117944049664',
+            id: '895602909390209034',
             type: 'ROLE',
             permission: true,
         },
     ];
 
     await command.permissions.set({ permissions });
+    console.log('Ready!')
+})
 
+client.on('interactionCreate', async interaction => {
+    if (!interaction.isCommand()) return;
 
     const { commandName } = interaction;
 
