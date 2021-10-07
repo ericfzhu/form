90a5b3bdf40829cabe1d656bfc3071001029656a
eric <73148494+ericzhu98@users.noreply.github.com>
2021-10-07 20:00:13 +1100

    added deploy command and slash command

--- src/deploy-commands.ts
+++ src/deploy-commands.ts
@@ -1,18 +1,21 @@
 const { SlashCommandBuilder } = require('@discordjs/builders');
 const { REST } = require('@discordjs/rest');
 const { Routes } = require('discord-api-types/v9');
-const { clientId, guildId, DISCORD_TOKEN } = require('dotenv').config();
+// @ts-ignore
+// const { clientId, guildId, token } = require('dotenv').config();
+const { clientId, guildId, token } = require('./config.json');
 
 const commands = [
     new SlashCommandBuilder().setName('form')
     .setDescription('Creates a Google form.')
-        .addStringOption(option => option.setName('input')
-        .setDescription('The input to echo back')
+    .addStringOption(option =>
+        option.setName('input')
+        .setDescription('The title of the form')
         .setRequired(true))
 ]
     .map(command => command.toJSON());
 
-const rest = new REST({ version: '9' }).setToken(DISCORD_TOKEN);
+const rest = new REST({ version: '9' }).setToken(token);
 
 rest.put(Routes.applicationGuildCommands(clientId, guildId), { body: commands })
     .then(() => console.log('Successfully registered application commands.'))
--- src/index.ts
+++ src/index.ts
@@ -1,11 +1,37 @@
-const { Client, Intents } = require('discord.js')
-const { token } = require('dotenv').config()
-// const { token } = require('./config.json');
+const { Client, Intents, Permissions } = require('discord.js')
+// @ts-ignore
+// const { clientId, guildId, token } = require('dotenv').config();
+const { token } = require('./config.json');
 
 const client = new Client({ intents: [Intents.FLAGS.GUILDS] })
 
 client.once('ready', () => {
     console.log('Ready!')
 })
+
+client.on('interactionCreate', async interaction => {
+    if (!interaction.isCommand()) return;
+
+    if (!client.application?.owner) await client.application?.fetch();
+
+    const command = await client.guilds.cache.get('123456789012345678')?.commands.fetch('876543210987654321');
+
+    const permissions = [
+        {
+            id: '201930117944049664',
+            type: 'ROLE',
+            permission: true,
+        },
+    ];
+
+    await command.permissions.set({ permissions });
+
+
+    const { commandName } = interaction;
+
+    if (commandName === 'form') {
+        await interaction.reply('Pong!');
+    }
+});
 
 client.login(token)
