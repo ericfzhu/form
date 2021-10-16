1b161ca1f28192af6d1c74a1acffbc0a330e29a2
eric <73148494+ericzhu98@users.noreply.github.com>
2021-10-17 04:42:39 +1100

    Testing googleapi scripts package

--- src/index.ts
+++ src/index.ts
@@ -1,4 +1,7 @@
 import {drive} from "googleapis/build/src/apis/drive";
+import {oauth2_v2} from "googleapis";
+import Oauth2 = oauth2_v2.Oauth2;
+import moment = require("moment");
 
 const { Client, Intents, Permissions } = require('discord.js')
 // @ts-ignore
@@ -7,13 +10,15 @@ const { token } = require('./config.json');
 const axios = require('axios').default;
 
 const client = new Client({ intents: [Intents.FLAGS.GUILDS] })
-
 const fs = require('fs');
 const readline = require('readline');
 const {google} = require('googleapis');
+const { GoogleAuth } = require('google-auth-library');
 
 // If modifying these scopes, delete token.json.
-const SCOPES = ['https://www.googleapis.com/auth/drive.metadata'];
+const SCOPES = ['https://www.googleapis.com/auth/script.projects',
+    'https://www.googleapis.com/auth/forms',
+    'https://www.googleapis.com/auth/drive'];
 // The file token.json stores the user's access and refresh tokens, and is
 // created automatically when the authorization flow completes for the first
 // time.
@@ -22,41 +27,25 @@ const TOKEN_PATH = 'token.json';
 // Load client secrets from a local file.
 fs.readFile('credentials.json', (err, content) => {
     if (err) return console.log('Error loading client secret file:', err);
-    // Authorize a client with credentials, then call the Google Drive API.
-    authorize(JSON.parse(content), listFiles);
+    // Authorize a client with credentials, then call the Google Apps Script API.
+    authorize(JSON.parse(content));
 });
 
-const {client_secret, client_id, redirect_uris} = JSON.parse(fs.readFile('credentials.json')).installed;
-const oAuth2Client = new google.auth.OAuth2(
-    client_id, client_secret, redirect_uris[0]);
-
-
-const drive = google.drive({
-    version: 'v3',
-    auth: oAuth2Client,
-})
 /**
  * Create an OAuth2 client with the given credentials, and then execute the
  * given callback function.
  * @param {Object} credentials The authorization client credentials.
- * @param {function} callback The callback to call with the authorized client.
  */
-function authorize(credentials, callback) {
+function authorize(credentials) {
     const {client_secret, client_id, redirect_uris} = credentials.installed;
     const oAuth2Client = new google.auth.OAuth2(
         client_id, client_secret, redirect_uris[0]);
 
-
-    const drive = google.drive({
-        version: 'v3',
-        auth: oAuth2Client,
-    })
-
     // Check if we have previously stored a token.
     fs.readFile(TOKEN_PATH, (err, token) => {
-        if (err) return getAccessToken(oAuth2Client, callback);
+        if (err) return getAccessToken(oAuth2Client);
         oAuth2Client.setCredentials(JSON.parse(token));
-        callback(oAuth2Client);
+        return (oAuth2Client);
     });
 }
 
@@ -64,9 +53,8 @@ function authorize(credentials, callback) {
  * Get and store new token after prompting for user authorization, and then
  * execute the given callback with the authorized OAuth2 client.
  * @param {google.auth.OAuth2} oAuth2Client The OAuth2 client to get token for.
- * @param {getEventsCallback} callback The callback for the authorized client.
  */
-function getAccessToken(oAuth2Client, callback) {
+function getAccessToken(oAuth2Client) {
     const authUrl = oAuth2Client.generateAuthUrl({
         access_type: 'offline',
         scope: SCOPES,
@@ -86,34 +74,28 @@ function getAccessToken(oAuth2Client, callback) {
                 if (err) return console.error(err);
                 console.log('Token stored to', TOKEN_PATH);
             });
-            callback(oAuth2Client);
+            return (oAuth2Client);
         });
     });
 }
 
 /**
- * Lists the names and IDs of up to 10 files.
+ * Creates a new script project, upload a file, and log the script's URL.
  * @param {google.auth.OAuth2} auth An authorized OAuth2 client.
+ * @param folderId
+ * @param folderName
  */
-function listFiles(auth) {
-    const drive = google.drive({version: 'v3', auth});
-    drive.files.list({
-        pageSize: 10,
-        fields: 'nextPageToken, files(id, name)',
-    }, (err, res) => {
-        if (err) return console.log('The API returned an error: ' + err);
-        const files = res.data.files;
-        if (files.length) {
-            console.log('Files:');
-            files.map((file) => {
-                console.log(`${file.name} (${file.id})`);
-            });
-        } else {
-            console.log('No files found.');
+function runAppsScript(auth, folderId, folderName) {
+    const script = google.script({version: 'v1', auth: auth});
+    const res = script.scripts.run({
+        scriptId: '1c3dQkYrnK0W_a20gj9cdYvvN3n-2ifGowcNSMqK88aXeUh10dKWrEtzQ',
+        requestBody: {
+            'function': 'doGet',
+            'paremeters': [folderId, folderName]
         }
     });
+    return (res.data);
 }
-
 
 client.once('ready', async () => {
     if (!client.application?.owner) await client.application?.fetch();
@@ -138,10 +120,24 @@ client.on('interactionCreate', async interaction => {
     const { commandName } = interaction;
 
     if (commandName === 'form') {
-        const res = await drive.files.copy({
-            
-        })
-        const link = 'test link here'
+        let string = interaction.options.getString('input');
+        let date = moment().format('L');
+        date.replace("/", "-")
+        string = date + " " + string
+
+        console.log(string)
+        // Load client secrets from a local file.
+        const auth = fs.readFile('credentials.json', (err, content) => {
+            if (err) return console.log('Error loading client secret file:', err);
+            // Authorize a client with credentials, then call the Google Apps Script API.
+            return authorize(JSON.parse(content));
+        });
+
+        // Acquire an auth client, and bind it to all future calls
+        // const authClient = await auth.getClient();
+        // google.options({auth: authClient});
+
+        const link = runAppsScript(auth, '1GN47fE8lUdv3ItcKIr6sAlb1hsftxVkO', string)
         await interaction.reply(link);
     }
 });
