da0a989ebd4b24db5c650d3fcaeb7eade4a0866d
eric <73148494+ericzhu98@users.noreply.github.com>
2021-11-25 19:22:13 +1100

    Cleaning up deploy-commands.js file

--- src/deploy-commands.js
+++ src/deploy-commands.js
@@ -1,8 +1,6 @@
 const { SlashCommandBuilder } = require("@discordjs/builders");
 const { REST } = require("@discordjs/rest");
 const { Routes } = require("discord-api-types/v9");
-// @ts-ignore
-// const { clientId, guildId, token } = require('dotenv').config();
 const { clientId, guildId, token } = require("./config.json");
 
 const commands = [
