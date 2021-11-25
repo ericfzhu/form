ba934f43be0375eb4e10932686ed728f6ed8a9d9
eric <73148494+ericzhu98@users.noreply.github.com>
2021-11-25 19:03:06 +1100

    Cleaning up index file

--- src/index.js
+++ src/index.js
@@ -1,27 +1,25 @@
-const { Client, Intents, Permissions } = require("discord.js");
-// @ts-ignore
-// const { clientId, guildId, token } = require('dotenv').config();
-const { token } = require("./config.json");
-
+const { Client, Intents } = require("discord.js");
 const client = new Client({ intents: [Intents.FLAGS.GUILDS] });
 const moment = require("moment");
-const express = require('express');
+const { token } = require("./config.json");
 const { spawn } = require('child_process');
 const wait = require('util').promisify(setTimeout);
 
 client.once("ready", async () => {
   if (!client.application?.owner) await client.application?.fetch();
-
+  // get(guildId)?.commands.fetch(commandId);
   const command = await client.guilds.cache
     .get("157263595128881153")
     ?.commands.fetch("912871983488839720");
 
   const permissions = [
+      // Feature testing role ID
     {
       id: "895602909390209034",
       type: "ROLE",
       permission: true,
     },
+      // PCSoc exec role ID
     {
       id: "201930117944049664",
       type: "ROLE",
