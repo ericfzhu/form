bdaf3807a45741f24ec997cb6fe17d9e3bf726bb
Eric Zhu <73148494+ericfzhu@users.noreply.github.com>
2022-04-02 20:16:14 +1100

    Applied code style rules

--- src/deploy-commands.ts
+++ src/deploy-commands.ts
@@ -1,31 +1,38 @@
-const { SlashCommandBuilder } = require("@discordjs/builders");
-const { REST } = require("@discordjs/rest");
-const { Routes } = require("discord-api-types/v9");
+const { SlashCommandBuilder } = require('@discordjs/builders');
+const { REST } = require('@discordjs/rest');
+const { Routes } = require('discord-api-types/v9');
 require('dotenv').config();
 
 const commands = [
   new SlashCommandBuilder()
-    .setName("form")
-    .setDescription("Creates a Google form.")
-    .addStringOption((option) =>
-      option
-        .setName("input")
-        .setDescription("The title of the form")
-        .setRequired(true)
-    )
+    .setName('form')
+    .setDescription('Creates a Google form.')
+    .addStringOption((option) => option
+      .setName('input')
+      .setDescription('The title of the form')
+      .setRequired(true))
     .setDefaultPermission(false),
 ].map((command) => command.toJSON());
 
-const rest = new REST({ version: "9" }).setToken(process.env.DISCORD_TOKEN);
+const rest = new REST({ version: '9' }).setToken(process.env.DISCORD_TOKEN);
 
 rest
-  .put(Routes.applicationGuildCommands(process.env.CLIENT_ID, process.env.GUILD_ID), { body: commands })
-  .then(() => console.log("Successfully registered application commands."))
+  .put(
+    Routes.applicationGuildCommands(
+      process.env.CLIENT_ID,
+      process.env.GUILD_ID,
+    ),
+    { body: commands },
+  )
+  .then(() => console.log('Successfully registered application commands.'))
   .catch(console.error);
 
 async function getfunction() {
   const guildCommands = await rest.get(
-    Routes.applicationGuildCommands(process.env.CLIENT_ID, process.env.GUILD_ID)
+    Routes.applicationGuildCommands(
+      process.env.CLIENT_ID,
+      process.env.GUILD_ID,
+    ),
   );
   console.log(guildCommands);
 }
--- src/index.ts
+++ src/index.ts
@@ -1,46 +1,47 @@
-const { Client, Intents } = require("discord.js");
+const { Client, Intents } = require('discord.js');
+
 const client = new Client({ intents: [Intents.FLAGS.GUILDS] });
-const moment = require("moment");
+const moment = require('moment');
 const { spawn } = require('child_process');
 require('dotenv').config();
 
 const axios = require('axios').default;
 
-client.once("ready", async () => {
+client.once('ready', async () => {
   if (!client.application?.owner) await client.application?.fetch();
   // get(guildId)?.commands.fetch(commandId);
   const command = await client.guilds.cache
     .get(process.env.GUILD_ID)
-    ?.commands.fetch("912871983488839720");
+    ?.commands.fetch('912871983488839720');
 
   const permissions = [
-      // Feature testing role ID
+    // Feature testing role ID
     {
-      id: "895602909390209034",
-      type: "ROLE",
+      id: '895602909390209034',
+      type: 'ROLE',
       permission: true,
     },
-      // PCSoc exec role ID
+    // PCSoc exec role ID
     {
       id: process.env.ROLE_ID,
-      type: "ROLE",
+      type: 'ROLE',
       permission: true,
     },
   ];
 
   await command.permissions.set({ permissions });
-  console.log("Command permissions set");
+  console.log('Command permissions set');
 });
 
-client.on("interactionCreate", async (interaction) => {
+client.on('interactionCreate', async (interaction) => {
   if (!interaction.isCommand()) return;
 
   const { commandName } = interaction;
 
-  if (commandName === "form") {
-    let string = interaction.options.getString("input");
-    let date = moment().format("L");
-    date = date.replaceAll("/", "-");
+  if (commandName === 'form') {
+    const string = interaction.options.getString('input');
+    let date = moment().format('L');
+    date = date.replaceAll('/', '-');
 
     // let url = 'https://script.googleapis.com/v1/scripts/AKfycbw1TaLKmd24DnaY9aDVLr_XSvOtEOKORQLfmBHvDpNyvou4YNbZ5cxCBXttyNBYiQI:run?folderId';
     // axios({
@@ -54,22 +55,21 @@ client.on("interactionCreate", async (interaction) => {
     //   console.log(link);
     // });
 
-
     await interaction.reply('Generating link...');
     let link;
     // Spawns a child process to run the python script
     const python = spawn('python', ['script.py', date, string]);
 
     // Collect response from script
-    await python.stdout.on('data', function (data) {
+    await python.stdout.on('data', (data) => {
       link = data.toString();
       console.log(link);
     });
 
     // Edit original message with script response after script closes
-    await python.on('close', (code => {
+    await python.on('close', () => {
       interaction.editReply(String(link));
-    }))
+    });
   }
 });
 
