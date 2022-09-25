692cd271c44da7773329716f7cabfda56a30405a
Eric Zhu <73148494+ericfzhu@users.noreply.github.com>
2022-09-26 00:17:29 +1000

    Setup Discord endpoint with Nextcord

--- src/main.py
+++ src/main.py
@@ -1,7 +1,11 @@
-import nextcord
-from nextcord.ext import commands
-from dotenv import load_dotenv
 import os
+from datetime import datetime
+from typing import Optional
+
+import nextcord
+from dotenv import load_dotenv
+from nextcord import SlashOption
+from nextcord.ext import commands
 
 load_dotenv()
 DISCORD_BOT_TOKEN = os.environ['DISCORD_BOT_TOKEN']
@@ -9,9 +13,16 @@ DISCORD_BOT_TOKEN = os.environ['DISCORD_BOT_TOKEN']
 bot = commands.Bot()
 
 
-@bot.slash_command(description="Creates an Arc Attendance Form.")
-async def form_builder(interaction: nextcord.Interaction):
-    await interaction.send("test", ephemeral=True)
+@bot.slash_command(description="Creates an Arc Attendance Form.", guild_ids=[860941635047522374])
+async def form(
+        interaction: nextcord.Interaction,
+        name: str = SlashOption(required=True, description='Name of the event.'),
+        date: Optional[str] = SlashOption(
+            required=False,
+            description='Date of the event.',
+            default=datetime.today().strftime('%Y-%m-%d'))):
+
+    await interaction.send(f'{date} {name}')
 
 
 bot.run(DISCORD_BOT_TOKEN)
