6a3e1e9ccf6190ef528e8dd34bdc4d7866db1c09
Eric Zhu <73148494+ericfzhu@users.noreply.github.com>
2022-09-26 23:49:41 +1000

    Ported old script into new google_forms endpoint module

--- endpoints/google_forms.py
+++ endpoints/google_forms.py
@@ -1,7 +1,40 @@
 # Creates an Arc Attendance Form on Google
+from pydrive2.auth import GoogleAuth
+from pydrive2.drive import GoogleDrive
+import sys
+import os
+from dotenv import load_dotenv
+
+gauth = GoogleAuth()
+gauth.LocalWebserverAuth()
+drive = GoogleDrive(gauth)
+
+load_dotenv()
+TEAM_DRIVE_ID = os.getenv('TEAM_DRIVE_ID')
+PARENT_FOLDER_ID = os.getenv('PARENT_FOLDER_ID')
+FORM_ID = os.getenv('FORM_ID')
 
 def post(name, date):
+    folder = drive.CreateFile({
+        'title': f'{date} {name}',
+        'mimeType': 'application/vnd.google-apps.folder',
+        'parents': [{
+            'kind': 'drive#fileLink',
+            'id': PARENT_FOLDER_ID,
+            'teamDriveId': TEAM_DRIVE_ID
+        }]
+    })
+    folder.Upload(param={'supportsAllDrives': True})
 
-    url = 'https://www.google.com/'
+    form = drive.auth.service.files().copy(
+        fileId=FORM_ID,
+        body={
+            'parents': [{
+                'kind': 'drive#fileLink',
+                'id': folder['id'],
+            }],
+            'title': 'Arc Online Event Attendance List',
+        }
+    ).execute()
 
-    return url
+    return f'https://docs.google.com/forms/d/{form["id"]}/viewform'
