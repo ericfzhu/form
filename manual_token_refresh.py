ac61249df73f7752fa0c0acbdfb5e77409618383
Eric Zhu <73148494+ericfzhu@users.noreply.github.com>
2022-10-04 13:06:02 +1100

    Updated manual token refresh workflow

--- manual_token_refresh.py
+++ manual_token_refresh.py
@@ -1,7 +1,10 @@
 import os
 from pydrive2.auth import GoogleAuth
 
-os.remove("credentials.json")
+# Remove existing credentials if it exists
+if os.path.exists("credentials.json"):
+    os.remove("credentials.json")
 
+# Reauth with Google
 gauth = GoogleAuth()
-gauth.CommandLineAuth()
+gauth.LocalWebserverAuth()
