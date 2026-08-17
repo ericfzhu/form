75bc6bff5704ad455381bd261254eebad753f7f7
Eric Zhu <73148494+ericfzhu@users.noreply.github.com>
2026-08-17 19:46:25 +1000

    Use a single Vite entry

--- vite.config.js
+++ vite.config.js
@@ -1,15 +1,6 @@
-import { resolve } from "node:path";
 import { defineConfig } from "vite";
 import tailwindcss from "@tailwindcss/vite";
 
 export default defineConfig({
   plugins: [tailwindcss()],
-  build: {
-    rollupOptions: {
-      input: {
-        main: resolve(process.cwd(), "index.html"),
-        demo: resolve(process.cwd(), "demo/index.html"),
-      },
-    },
-  },
 });
